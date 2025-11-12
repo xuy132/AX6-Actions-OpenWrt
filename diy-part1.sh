114dffaf9778ca17dc32e7b7304172b3640d4e0d
xuxihan <61369344+xuy132@users.noreply.github.com>
2025-11-12 08:18:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,18 +24,43 @@ sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/file
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
-#echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
-#echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass' >>feeds.conf.default
-echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns -b v5' >>feeds.conf.default
-echo 'src-git taskplan  https://github.com/sirpdboy/luci-app-taskplan' >> feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >> feeds.conf.default
-#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-#echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
-#echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
-#echo 'src-git Boos https://github.com/Boos4721/OpenWrt-Packages' >> feeds.conf.default
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
+}
+function drop_package(){
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+}
 
+rm -rf package/custom; mkdir package/custom
+
+# git clone https://github.com/fw876/helloworld.git package/ssr
+git clone https://github.com/firker/diy-ziyong -b 2305 package/diy-ziyong
+# merge_package https://github.com/firker/diy-ziyong diy-ziyong/wrtbwmon
+# git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
+# git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git  package/luci-theme-kucat
+# git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/openwrt-nikki
+# git clone https://github.com/timsaya/openwrt-bandix  package/openwrt-bandix 
+# git clone https://github.com/timsaya/luci-app-bandix  package/luci-app-bandix 
+# git clone https://github.com/sirpdboy/luci-app-netspeedtest  package/luci-app-netspeedtest
+# git clone https://github.com/Thaolga/luci-app-nekoclash  package/luci-app-nekoclash
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
+git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
+# git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-serverchan
+# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+chmod 755 ./package/lucky/luci-app-lucky/root/usr/bin/luckyarch
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
+# git clone https://github.com/linkease/nas-packages-luci.git package/nas-packages-luci
+# git clone https://github.com/linkease/nas-packages.git package/nas-packages
+git clone https://github.com/linkease/istore.git package/istore
+# sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci/luci-app-store/Makefile
